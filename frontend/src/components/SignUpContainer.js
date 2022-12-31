fc8d501777c0173c8f9d0deed35791d6ae68961e
taous <kt_habbak@esi.dz>
2022-12-31 12:45:00 +0100

    Signup page

---
+++ frontend/src/components/SignUpContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './SignUpContainer.css'
+import { useEffect , useState } from 'react'
+
+
+function LoginContainer(props) {
+    return (
+    <div className='container'>
+     <div className='grid-container'>
+        <div className='colonne-1'/>
+        <div className='colonne-2'> 
+          <h2> Inscrivez-vous et trouvez votre bonheur !</h2> 
+          <button className='btn--login' onClick={() => props.onFormSwitch('register')}>se connecter avec google</button>
+          <Link to="login" className='lien'>je possède un compte</Link>
+        </div>
+     </div>
+   </div>
+ )
+
+ 
+}
+
+export default LoginContainer
