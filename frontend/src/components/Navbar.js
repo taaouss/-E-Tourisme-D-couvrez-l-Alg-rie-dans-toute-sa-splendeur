f9b92133253e612319e371ce1378662622952c93
thezanis11 <101350540+thezanis11@users.noreply.github.com>
2023-01-03 18:30:53 +0100

    quelques corrections

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -52,7 +52,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/products'
+                to='/sign-in'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
