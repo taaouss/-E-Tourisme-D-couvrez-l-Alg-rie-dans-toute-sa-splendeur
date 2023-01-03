8444cc681c1acf3fe1d29e91009c7d2be0615061
Melissasadou <Km_sadou@esi.dz>
2023-01-03 23:16:22 +0100

    views et urls 1

---
+++ app/serializer.py
@@ -0,0 +1,40 @@
+from django.forms import IntegerField
+from rest_framework import serializers
+from app.models import Annonce , Client
+
+class AnnonceSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only = True)
+    surface = serializers.FloatField(allow_null=False)
+    prix = serializers.IntegerField(allow_null=False)
+    description = serializers.CharField(allow_blank=True, allow_null=True)
+    date = serializers.DateTimeField(read_only = True)
+    pos = serializers.CharField(allow_null=False)
+    photos = serializers.CharField(allow_null=False)
+    categories = serializers.CharField(allow_null=False)
+    types = serializers.CharField(allow_null=False)
+    
+    def create(self , data):
+        return Annonce.objects.create(**data)
+    
+    def update(self, instance, data):
+        instance.surface = data.get('surface', instance.surface)
+        instance.prix = data.get('prix', instance.prix)
+        instance.description = data.get('description', instance.description)
+        instance.pos = data.get('pos', instance.pos)
+        instance.photos = data.get('photos', instance.photos)
+        instance.categories = data.get('categories', instance.categories)
+        instance.types = data.get('types', instance.types)
+        instance.save()
+        return instance
+        
+    
+    
+class ClientSerializer(serializers.Serializer):
+    email = serializers.EmailField(allow_null=False)
+    nom = serializers.CharField(allow_null=False)
+    prenom = serializers.CharField(allow_null=False)
+    telephone = serializers.IntegerField(allow_null=False)
+    annonces = serializers.CharField(allow_null=True)
+    
+    #def create(self , data):
+      #  return Client.objects.create(**data)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from app.models import Annonce , Position , Client , Photo
+from app.serializer import AnnonceSerializer , ClientSerializer 
 # Create your views here.
+@api_view(['GET'])
+def Annonce_list(request):
+    annonces = Annonce.objects.all()
+    lesannonces = AnnonceSerializer(annonces, many= True)
+    return Response(lesannonces.data)
+
+@api_view(['POST'])
+def Annonce_creat(request):
+    lesannonces = AnnonceSerializer(data= request.data)
+    if lesannonces.is_valid():
+        lesannonces.save()
+        return Response(lesannonces.data)
+    else :
+        return Response(lesannonces.errors)
+    
+@api_view(['POST', 'PUT', 'DELETE'])  
+def annonce(request, pk):
+    annonce = Annonce.objects.get(pk=pk)
+    if request.method == 'GET':
+        lesannonces = AnnonceSerializer(annonce)
+        return Response(lesannonces.data)
+    if request.method == 'PUT':
+        lesannonces = AnnonceSerializer(annonce, data=request.data)
+        if lesannonces.is_valid():
+            lesannonces.save()
+            return Response(lesannonces.data)
+        return lesannonces.errors
+    if request.method == 'DELETE':
+        annonce.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+   
+   
+@api_view(['GET'])
+def Client_list(request):
+    clients = Client.objects.all()
+    lesclients = ClientSerializer(clients , many=True)
+    return Response(lesclients.data)
+
+     
+@api_view(['POST'])
+def Client_creat(request):
+    lesclients = ClientSerializer(data= request.data)
+    if lesclients.is_valid():
+        lesclients.save()
+        return Response(lesclients.data)
+    else :
+        return Response(lesclients.errors)
+    
+@api_view(['POST', 'PUT', 'DELETE'])  
+def client(request, pk):
+    client = Client.objects.get(pk=pk)
+    if request.method == 'GET':
+        lesclients = ClientSerializer(client)
+        return Response(lesclients.data)
+    if request.method == 'PUT':
+        lesclients = ClientSerializer(client , data=request.data)
+        if lesclients.is_valid():
+            lesclients.save()
+            return Response(lesclients.data)
+        return lesclients.errors
+    if request.method == 'DELETE':
+        client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
